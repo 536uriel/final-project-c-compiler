364b7a95b2454a28800b7e25c826971da8432ede
536uriel <uribrave1@gmail.com>
2023-06-30 11:11:12 +0300

    isWordMatch function

--- main.c
+++ main.c
@@ -2,9 +2,31 @@
 #include <string.h>
 #include "var.c"
 
+#define FALSE 0;
+#define TRUE 1;
+
 char input[1000];
 long code[1024];
 long wordCounter = 0;
+
+int isWordMatch(int i,char word[])
+{
+   int index;
+   for(index = 0;index < strlen(word);index++)
+   {
+      if(input[i] != word[index])
+      {
+         return FALSE;
+      }
+
+      if(index == strlen(word))
+      {
+         return TRUE;
+      }
+
+      i++;
+   }
+}
 
 
 int main() {
@@ -25,7 +47,7 @@ int main() {
       i++;
    }
 
-   printf("%d",i);
+   // printf("%d",i);
    
    return 0;
 }
